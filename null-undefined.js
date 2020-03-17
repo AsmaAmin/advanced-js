91d97c8dbc2ccd854fd3624009cb27f755453a88
Asma <user@DESKTOP-KIPN1TO>
2020-03-17 11:01:54 +0600

    true vs False and Undefined vs Null done

---
+++ null-undefined.js
@@ -0,0 +1,30 @@
+///**Undefined */
+//1
+let pakhi;
+console.log(pakhi);
+//2
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+const result = add(13 , 82);
+console.log(result);
+
+//3
+function add(num1, num2){
+console.log(num1, num2);
+}
+const result1 = add(13);
+console.log(result1);
+
+
+//4
+const premik = {name : "Smart", phone:4456}
+console.log(premik.gf);
+
+//5
+let fun = undefined;
+console.log(fun);
+
+//**Null */
+//Nul is not exist. null explicitle set or empty.
---
+++ true-false.js
@@ -0,0 +1,17 @@
+//**Falsy */
+//0
+//""
+//undefined
+//null
+//Nan
+
+
+//**Truthy */
+//0, ' ', 
+
+const name = undefined
+if(name){
+    console.log("|true")
+}else{
+    console.log("False")
+}
